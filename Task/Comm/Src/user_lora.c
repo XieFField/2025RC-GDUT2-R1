85541d468bf845a1099fd32ea199e50165c959b9
AdamDream <1192241289@qq.com>
2025-07-17 15:47:43 +0800

    速度规划以及若干锁定状态管理

--- Task/Comm/Src/user_lora.c
+++ Task/Comm/Src/user_lora.c
@@ -28,7 +28,7 @@ struct __FILE
 	int handle; 
 }; 
 
-FILE __stdout;      
+//FILE __stdout;      
 
 //定义_sys_exit()以避免使用半主机模式    
 void _sys_exit(int x) 
---
+++ 速度规划以及若干锁定状态管理/2025RC-GDUT2-R1-main/Task/User/Inc/LED.h
@@ -0,0 +1,38 @@
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef LED_H
+#define LED_H
+
+#pragma once
+#include "drive_ws2812_SPI.h"
+
+
+void LED_Init(void);
+
+/**
+ * @brief Position故障为0~3索引灯
+ */
+void LED_Position_Error(void);
+
+void LED_Positoin_Properly(void);
+
+/**
+ * CANfifo故障为5~7索引的灯
+ */
+void LED_CANfifo_Error(void);
+
+void LED_CANfifo_Properly(void);
+
+void LED_other(void);
+
+void LED_ALLlight(void);
+
+#endif
+#ifdef __cplusplus
+}
+
+
+#endif
