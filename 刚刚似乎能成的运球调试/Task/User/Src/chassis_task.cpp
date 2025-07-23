801773b1da7e7a6c5f49de46a77640ca475b8fa4
AdamDream <1192241289@qq.com>
2025-07-23 17:56:15 +0800

    运球

---
+++ 刚刚似乎能成的运球调试/Task/User/Src/chassis_task.cpp
@@ -0,0 +1,370 @@
+/**
+ * @file chassis_task.cpp
+ * @author Wu Jia
+ * @brief 机构任务调用
+ * @version 1.8218319532
+ * @attention 由于和视觉建图，所以代码里保留了不少测试功能，可视情况修改
+ */
+#include <cmath> // Add this at the top of the file if not already included
+#include "chassis_task.h"
+#include "speed_plan.h"
+#include "shoot.h"
+#include "position.h"
+#include "drive_uart.h"
+#include "LaserPositioning_Task.h"
+#include "ViewCommunication.h"
+#include "drive_uart.h"
+
+extern float Laser_Y_return;
+extern float Laser_X_return;
+
+
+int32_t speed1; //用于测试
+int32_t speed2;
+int32_t speed3;
+
+Ws2812b_SIGNAL_T send_signal = SIGNAL_WAIT;
+
+
+PID_T yaw_pid = {0};
+PID_T point_X_pid = {0};
+PID_T point_Y_pid = {0};
+float shootacc = 45000;
+Omni_Chassis chassis(0.152/2.f, 0.442f/2.f, 3, 1.f); //底盘直径0.442m，轮子半径0.152m，底盘加速度0.5m/s^2
+Launcher launch(1180.f,-1320.645996, shootacc); //俯仰最大角度 推球最大角度 摩擦轮加速度限幅 shootacc rpm/s^2
+CONTROL_T ctrl;
+float lock_angle = 0;
+float target_speed = 50000;
+float HOOP_X = -5.56530714f;
+float HOOP_Y = -0.112568647f;
+float test_auto = 120.0f;
+
+float catch_openAngle = 4500.0f;
+
+float auto_pitch = 0.0f;
+
+// uint8_t test_buff[17] = {0};
+
+ShootController SHOOT;  //投篮拟合对象
+ShootController::Shoot_Info_E shoot_info = {0};
+float pitch_level = 1;  //1 2 3 分别对应 近 中 远
+
+// 模拟小仰角样条数据
+const ShootController::SplineSegment smallPitchTable[] = {
+    {-42999.4884f, 27008.1149f, 9081.7162f, 35500.0000f},
+    {-42999.4884f, -81.5628f, 14736.2921f, 38200.0000f},
+    {78313.0705f, -25881.2559f, 9543.7284f, 40800.0000f},
+    {-57752.7935f,  21106.5864f, 8588.7945f, 42300.0000f},
+    {65198.1036f,  -13545.0897f, 10101.0938f, 44400.0000f},
+    {-53039.6207f, 25573.7724f, 12506.8303f, 46400.0000f},
+    {-53039.6207f, -6250.0000f, 16371.5848f, 49500.0000f},
+};
+
+const float smallPitchDistances[] = {1.2f, 1.4f, 1.6f, 1.8f, 2.0f, 2.2f, 2.4f, 2.6f};
+
+// 模拟中仰角样条数据
+const ShootController::SplineSegment midPitchTable[] ={
+    {1.0f, 0.0f, 0.0f, 0.0f},
+    {1.2f, 0.0f, 0.0f, 0.0f},
+    {1.4f, 0.0f, 0.0f, 0.0f},
+    {1.6f, 0.0f, 0.0f, 0.0f},
+    {1.8f, 0.0f, 0.0f, 0.0f},
+    {2.0f, 0.0f, 0.0f, 0.0f} 
+};
+
+const float midPitchDistances[] = {2.6f, 2.8f, 3.0f, 3.2f, 3.4f, 3.6f, 3.8f};
+
+ // 模拟大等仰角样条数据
+const ShootController::SplineSegment largePitchTable[] = {
+    {1.0f, 0.0f, 0.0f, 0.0f},
+    {1.2f, 0.0f, 0.0f, 0.0f},
+    {1.4f, 0.0f, 0.0f, 0.0f},
+    {1.6f, 0.0f, 0.0f, 0.0f},
+    {1.8f, 0.0f, 0.0f, 0.0f},
+    {2.0f, 0.0f, 0.0f, 0.0f},
+    {2.2f, 0.0f, 0.0f, 0.0f},
+    {2.4f, 0.0f, 0.0f, 0.0f}
+};
+
+const float largePitchDistances[] = {3.8f, 4.0f, 4.2f, 4.4f, 4.6f, 4.8f, 5.0f};
+
+void LED_InfoSend(void);
+
+/**
+ * @brief 更新pitch_level，后续考虑封装到ShootController的类中，
+ *        但目前封进去有点小麻烦，而且也不影响使用
+ */
+int UpdatePitchLevel(float distance, int current_level)
+{
+    // 俯仰切换临界值，来自各Pitch的距离样本点
+    // const float SMALL_TO_MID = 3.6f;  // smallPitchDistances[6]
+    // const float MID_TO_SMALL = 2.4f;  // smallPitchDistances[0]
+    // const float MID_TO_LARGE = 3.8f;  // midPitchDistances[1]
+    // const float LARGE_TO_MID = 3.6f;  // midPitchDistances[0]
+
+    const float SMALL_TO_MID = smallPitchDistances[7];  // = 3.6f
+    const float MID_TO_SMALL = midPitchDistances[0];  // = 2.6f
+    const float MID_TO_LARGE = midPitchDistances[6];    // = 3.8f
+    const float LARGE_TO_MID = largePitchDistances[0];    // = 3.6f
+
+    switch (current_level)
+    {
+        case 1: // 小仰角
+            if (distance > SMALL_TO_MID)
+                return 2;
+            return 1;
+
+        case 2: // 中仰角
+            if (distance < MID_TO_SMALL)
+                return 1;
+            if (distance > MID_TO_LARGE)
+                return 3;
+            return 2;
+
+        case 3: // 大仰角
+            if (distance < LARGE_TO_MID)
+                return 2;
+            return 3;
+
+        default:
+            return 1;
+    }
+}
+
+int i = 0;
+void Chassis_Task(void *pvParameters)
+{
+    /*
+    // 初始化大仰角样条数据
+    SHOOT.Init(largePitchTable, largePitchDistances, sizeof(largePitchDistances)/sizeof(float), 3);
+
+    // 初始化中仰角样条数据
+    SHOOT.Init(midPitchTable, midPitchDistances, sizeof(midPitchDistances)/sizeof(float), 2);
+    */
+    // 初始化小仰角样条数据
+    SHOOT.Init(smallPitchTable, smallPitchDistances, sizeof(smallPitchDistances)/sizeof(float), 1);
+    
+
+    static uint8_t Laser_Data = 0x00;
+
+    static bool relocate_on = false;
+    xQueueSend(LED_Port, &send_signal, pdTRUE);
+    for(;;)
+    {   
+
+        /*用于测试*/
+        speed1 = launch.FrictionMotor[0].get_speed();
+        speed2 = -launch.FrictionMotor[1].get_speed();
+        speed3 = launch.FrictionMotor[2].get_speed();
+        if (speed1 < 0) 
+        {
+            (uint32_t)speed1 += 65546;
+        } 
+        if (speed2 < 0) 
+        {
+            (uint32_t)speed2 += 65546;
+        } 
+        if (speed3 < 0) 
+        {
+            (uint32_t)speed3 += 65546;
+        } 	
+        /*用于测试*/
+
+      if(xQueueReceive(Chassia_Port, &ctrl, pdTRUE) == pdPASS)
+      {
+        
+        /*投篮数据获取*/
+        
+        SHOOT.GetShootInfo(HOOP_X, HOOP_Y, RealPosData.world_x, RealPosData.world_y, &shoot_info);
+        
+        pitch_level = UpdatePitchLevel(shoot_info.hoop_distance, pitch_level);
+
+        shoot_info.shoot_speed = SHOOT.GetShootSpeed(shoot_info.hoop_distance, pitch_level);
+        if(pitch_level == 1)
+            auto_pitch = 0.0f;
+        else if(pitch_level == 2)
+            auto_pitch = 90.0f;
+        else if(pitch_level == 3)
+            auto_pitch = 130.0f;
+        else
+            auto_pitch = 0.0f;
+            
+    
+        /*===========*/
+        printf_UART("%d,%d,%d\r\n",speed1,speed2,speed3);
+        /*==底盘控制==*/
+           if(ctrl.chassis_ctrl == CHASSIS_COM_MODE)
+           {
+               //普通控制模式
+               chassis.Control(ctrl.twist);
+           }
+           else if(ctrl.chassis_ctrl == CHASSIS_CALIBRA_MODE)
+           {
+               //激光校准模式
+               //还没做
+               Robot_Twist_t twist = {0};
+               chassis.Control(twist);
+           }
+           else if(ctrl.chassis_ctrl == CHASSIS_LOW_MODE) //低速模式
+           {
+                ctrl.twist.linear.x = ctrl.twist.linear.x * 0.3;
+                ctrl.twist.linear.y = ctrl.twist.linear.y * 0.3;
+                ctrl.twist.angular.z = ctrl.twist.angular.z * 0.3;
+                chassis.Control(ctrl.twist);
+           }
+           else if(ctrl.chassis_ctrl == CHASSIS_OFF)
+           {
+               //底盘关闭
+               Robot_Twist_t twist = {0};
+               chassis.Control(twist);
+           }
+           else if(ctrl.chassis_ctrl == CHASSIS_LOCK_TARGET)
+           {
+                 ctrl.twist.linear.x = ctrl.twist.linear.x * 0.15;
+                 ctrl.twist.linear.y = ctrl.twist.linear.y * 0.15;
+                 ctrl.twist.angular.z = ctrl.twist.angular.z;
+                chassis.Control(ctrl.twist);
+//               Robot_Twist_t twist = {0};
+//               chassis.Control(twist);
+           }
+           else
+           {
+               Robot_Twist_t twist = {0};
+               chassis.Control(twist);
+           }
+           /*=================================================================*/
+
+           /*==俯仰控制==*/
+           if(ctrl.pitch_ctrl == PITCH_HAND_MODE)
+               launch.PitchControl(0);
+
+           else if(ctrl.pitch_ctrl == PITCH_AUTO_MODE)
+               launch.Pitch_AutoCtrl(test_auto);
+
+           else if(ctrl.pitch_ctrl == PITCH_CATCH_MODE)
+               launch.Pitch_AutoCtrl(701);
+               
+           else if(ctrl.pitch_ctrl == PITCH_RESET_MODE)
+               launch.PitchControl(0);
+               
+           else if(ctrl.pitch_ctrl == PITCH_LOCK_MODE)
+           {
+                lock_angle = launch.LauncherMotor[0].get_angle();
+                launch.PitchControl(lock_angle);
+           }
+           else
+                launch.PitchControl(0);
+           /*==================================================================*/
+
+           /*==射球控制==*/
+           if(ctrl.friction_ctrl == FRICTION_OFF_MODE)
+           {
+               launch.ShootControl(false,false,0);
+           }
+           else if(ctrl.friction_ctrl == FRICTION_ON_MODE)
+           {
+               if(ctrl.shoot_ctrl == SHOOT_OFF)
+                  //launch.ShootControl(false,true,target_speed);
+                   //launch.ShootControl(false,true,shoot_info.shoot_speed);
+			   launch.DribbleControl(false,true,target_speed);
+               else
+                  // launch.ShootControl(true,true,target_speed);
+                   //launch.ShootControl(true,true,shoot_info.shoot_speed);
+			   launch.DribbleControl(true,true,target_speed);
+           }
+
+           /*===================================================================*/
+
+           /*接球机构控制*/
+           if(ctrl.catch_ball == CATCH_OFF)
+           {
+                launch.Catch_Ctrl_Spd(false,catch_openAngle);
+           }
+           else if(ctrl.catch_ball == CATCH_ON)
+           {
+                launch.Catch_Ctrl_Spd(true,catch_openAngle);
+           }
+           else
+           {
+                launch.Catch_Ctrl_Spd(false,catch_openAngle);
+               //CATCH_OFF 接球关闭
+           }
+           if(ctrl.laser_ctrl == LASER_CALIBRA_ON)
+            {
+               Reposition_SendData(Laser_X_return, Laser_Y_return);
+               Laser_Data = 0x01;
+               relocate_on = true;
+               xQueueSend(Enable_LaserModule_Port, &Laser_Data, pdTRUE);
+               //xQueueSend(Relocate_Port, &relocate_on, pdTRUE); //滤波
+            }
+            else if(ctrl.laser_ctrl == LASER_CALIBRA_OFF)
+            {
+               Laser_Data = 0x00;
+               relocate_on = false;
+               xQueueSend(Enable_LaserModule_Port, &Laser_Data, pdTRUE);
+             //  xQueueSend(Relocate_Port, &relocate_on, pdTRUE);
+            }
+            chassis.Motor_Control();
+            launch.LaunchMotorCtrl(); 
+            LED_InfoSend();
+       }
+        //printf_DMA("%f\r\n", target_speed);
+        //HAL_UART_Transmit_DMA(&huart1, test_buff, 17);
+       //ViewCommunication_SendByte();
+       
+        osDelay(1);
+    }
+}
+float kd_test=0.66;
+float kp_test=0.1;
+
+void PidParamInit(void)
+{       
+    chassis.Pid_Param_Init(0, 10.0f, 0.015f, 0.0f, 16384.0f, 16384.0f, 10); 
+    chassis.Pid_Param_Init(1, 10.0f, 0.015f, 0.0f, 16384.0f, 16384.0f, 10); 
+    chassis.Pid_Param_Init(2, 10.0f, 0.015f, 0.0f, 16384.0f, 16384.0f, 10); 
+
+    chassis.Pid_Mode_Init(0, 0.1f, 0.0f, false, true);
+    chassis.Pid_Mode_Init(1, 0.1f, 0.0f, false, true);
+    chassis.Pid_Mode_Init(2, 0.1f, 0.0f, false, true);
+
+    launch.Pid_Param_Init(0,15.0f, 0.015f, 0.0f, 3000.0f, 8000.0f, 0);
+    launch.Pid_Mode_Init(0,0.1f, 0.0f, false, true);
+
+    launch.Pid_Param_Init(1,12.0f, 0.015f, 0.0f, 16384.0f, 16384.0f, 0);
+    launch.Pid_Mode_Init(1,0.1f, 0.0f, false, true);
+
+    launch.Pid_Param_Init(2,12.0f, 0.015f, 0.0f, 10000.0f, 10000.0f, 0);
+    launch.Pid_Mode_Init(2,0.1f, 0.0f, false, true);
+
+    launch.Pid_Param_Init(3,12.0f, 0.015f, 0.0f, 10000.0f, 10000.0f, 0);
+    launch.Pid_Mode_Init(3,0.1f, 0.0f, false, true);
+
+//    //用于控制目标角度的角速度pid
+	pid_param_init(&yaw_pid, PID_Position, 1.5, 0.0f, 0, 0.2f, 360, kp_test, 0.0f, kd_test);
+//	
+//	//用于控制半径大小的法向速度pid
+    pid_param_init(&point_X_pid, PID_Position, 2.0, 0.0f, 0, 0.1f, 180.0f, 1.0f, 0.0f, 0.66f);
+}
+
+
+
+void LED_InfoSend(void)
+{
+    if(RealPosData.world_x == INFINITY || RealPosData.world_y == INFINITY 
+        || (RealPosData.world_x == 0 && RealPosData.world_y == 0 && RealPosData.world_yaw == 0))
+        // 无穷大、未成功接收数据报错
+        send_signal = SIGNAL_FAIL;
+
+    else if(ctrl.pitch_ctrl == PITCH_CATCH_MODE)
+        //接球
+        send_signal = SIGNAL_CATCH;
+
+    else if(ctrl.laser_ctrl == LASER_CALIBRA_ON)
+        send_signal = SIGNAL_WAIT;
+
+    else
+        send_signal = SIGNAL_NORMAL;
+    
+    xQueueSend(LED_Port, &send_signal, pdTRUE);
+}
---
+++ 刚刚似乎能成的运球调试/Task/User/Src/fsm_joy.cpp
@@ -0,0 +1,243 @@
+/**
+ * @file fsm_joy.cpp
+ * @author Wujia
+ * @brief 遥控状态机
+ * @version 0.1
+ * @date 2025-05-23
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include "fsm_joy.h"
+#include "drive_tim.h"
+#include "chassis_task.h"
+#include "speed_calculate.h"
+#include "lora.h"
+#include "LED.h"
+
+
+#define LASER_CALIBRA_YAW   0   //激光重定位时候车锁定的yaw轴数值
+
+void Air_Joy_Task(void *pvParameters)
+{
+    //LED_Init();
+
+    fsm_joy_timer.fsm_joy_timer_started = false;
+    fsm_joy_timer.fsm_joy_start_tick = 0;
+
+    static CONTROL_T ctrl;
+    for(;;)
+    {
+        //LED_ALLlight();
+        //遥杆消抖
+        // if(air_joy.LEFT_X>1400&&air_joy.LEFT_X<1600) 
+        //     air_joy.LEFT_X = 1500;
+        // if(air_joy.LEFT_Y>1400&&air_joy.LEFT_Y<1600)
+        //     air_joy.LEFT_Y = 1500;
+        if(air_joy.RIGHT_X>1400&&air_joy.RIGHT_X<1600)
+            air_joy.RIGHT_X = 1500;
+        if(air_joy.RIGHT_Y>1400&&air_joy.RIGHT_Y<1600)  
+            air_joy.RIGHT_Y = 1500;
+        //遥杆消抖
+		if(air_joy.LEFT_X>1400&&air_joy.LEFT_X<1600&&air_joy.LEFT_Y>1400&&air_joy.LEFT_Y<1600)
+		{
+			if (!fsm_joy_timer.fsm_joy_timer_started)
+            {
+                fsm_joy_timer.fsm_joy_start_tick = xTaskGetTickCount();
+                fsm_joy_timer.fsm_joy_timer_started = true;
+            }
+			if (xTaskGetTickCount() - fsm_joy_timer.fsm_joy_start_tick >= pdMS_TO_TICKS(200))
+            {
+				air_joy.LEFT_X = 1500;
+                air_joy.LEFT_Y = 1500;
+            }
+		}
+        else
+        {
+			// 重置定时器状态
+            fsm_joy_timer.fsm_joy_timer_started = false;
+            fsm_joy_timer.fsm_joy_start_tick = 0;
+		}
+
+
+        //遥控器启动判断
+        if(air_joy.LEFT_X!=0||air_joy.LEFT_Y!=0||air_joy.RIGHT_X!=0||air_joy.RIGHT_Y!=0)
+        {
+                
+            if(_tool_Abs(air_joy.SWB - 1000) > 400)
+            {                
+                ctrl.twist.linear.y = -(air_joy.LEFT_Y - 1500)/500.0 * 3.7;
+                ctrl.twist.linear.x = -(air_joy.LEFT_X - 1500)/500.0 * 3.7;
+                ctrl.twist.angular.z = (air_joy.RIGHT_X - 1500)/500.0 * 5;
+
+                ctrl.twist.pitch.column = (air_joy.RIGHT_Y - 1500)/500.0 * 2;
+
+                speed_world_calculate(&ctrl.twist.linear.x,&ctrl.twist.linear.y); 
+                /*======================================================*/
+                if(_tool_Abs(air_joy.SWB - 1500) < 50)//接球模式
+                {
+                    ctrl.robot_crtl = BALL_MODE;    
+                    //LED_CANfifo_Properly();
+                    
+                    if(_tool_Abs(air_joy.SWA - 1000) < 50) //SWA UP
+                    {
+                        ctrl.chassis_ctrl = CHASSIS_COM_MODE;   //普通移动
+                        ctrl.pitch_ctrl = PITCH_RESET_MODE;     //俯仰归位
+                        ctrl.catch_ball = CATCH_OFF;            //接球机构关闭
+                        ctrl.car_comm_ctrl = CAR_COMMUICA_OFF;   //双车通讯关闭
+                        if(_tool_Abs(air_joy.SWD - 1000) < 50)
+                        {
+                            ctrl.laser_ctrl = LASER_CALIBRA_OFF;
+                            
+                        }
+                        else if(_tool_Abs(air_joy.SWD - 2000) < 50)
+                        {
+                            ChassisYaw_Control(LASER_CALIBRA_YAW,&ctrl.twist.angular.z);  //用于锁定角度
+                            
+//                            speed_clock_basket_calculate(&ctrl.twist.angular.z);
+                            ctrl.laser_ctrl = LASER_CALIBRA_ON;
+                        }
+                    } 
+                    else if(_tool_Abs(air_joy.SWA - 2000) < 50) //SWA DOWN
+                    {
+                        if(_tool_Abs(air_joy.SWD - 1000) < 50)
+                        {
+                            ctrl.pitch_ctrl = PITCH_RESET_MODE;     //回起始位置
+                            ctrl.chassis_ctrl = CHASSIS_COM_MODE;   //普通移动
+                        }
+                        else if(_tool_Abs(air_joy.SWD - 2000) < 50)
+                        {
+                            ctrl.chassis_ctrl = CHASSIS_LOW_MODE;   //低速模式
+                            ctrl.pitch_ctrl = PITCH_CATCH_MODE;     //俯仰抬升接球
+                            ctrl.car_comm_ctrl = CAR_COMMUICA_ON;   //双车通讯开启
+                        }
+                       
+                        ctrl.catch_ball = CATCH_ON;             //接球机构开启  
+                        
+                    }
+                }
+                /*-========================================================-*/
+
+            #if Ring_or_ATUO_MODE    //定义在chassis_task.h中
+
+                else if(_tool_Abs(air_joy.SWB - 2000) < 50) //运动学方程方案
+                {
+                    ctrl.robot_crtl = SHOOT_MODE;   //射球模式
+                    if(_tool_Abs(air_joy.SWA - 2000) < 50)
+                    {
+                        ctrl.chassis_ctrl = CHASSIS_LOCK_TARGET;    //底盘锁定篮筐
+                        
+                        speed_clock_basket_calculate(&ctrl.twist.angular.z);                                             
+                    }
+                    else if(_tool_Abs(air_joy.SWA - 1000) < 50)
+                    {
+                        ctrl.chassis_ctrl = CHASSIS_LOW_MODE;       //底盘普通移动
+                    }
+
+                    
+                        if(_tool_Abs(air_joy.SWD - 1000) < 50)
+                        {
+                           // LED_Positoin_Properly();
+                            ctrl.pitch_ctrl = PITCH_HAND_MODE;          //俯仰手操
+                        }
+                        else if(_tool_Abs(air_joy.SWD - 2000) < 50)
+                        {
+                           // LED_Position_Error();
+                            ctrl.pitch_ctrl = PITCH_AUTO_MODE;          //俯仰自动
+                        }
+                    // else if(ctrl.chassis_ctrl == CHASSIS_COM_MODE)
+                    // {
+                    //     ctrl.pitch_ctrl = PITCH_HAND_MODE;
+                    //     ctrl.friction_ctrl = FRICTION_OFF_MODE;
+                    //     ctrl.shoot_ctrl = SHOOT_OFF;
+                    // }       
+                    
+                    if(ctrl.pitch_ctrl == PITCH_AUTO_MODE || ctrl.pitch_ctrl == PITCH_HAND_MODE)
+                    {   
+
+                        //当俯仰启用时才能启用摩擦轮
+                        if(_tool_Abs(air_joy.SWC - 1000) < 50)
+                        {
+                            ctrl.friction_ctrl = FRICTION_OFF_MODE;
+                            ctrl.shoot_ctrl = SHOOT_OFF;
+                        }
+                        else if(_tool_Abs(air_joy.SWC - 1500) < 50)
+                        {
+                            ctrl.friction_ctrl = FRICTION_ON_MODE;
+                            ctrl.shoot_ctrl = SHOOT_OFF;
+                        }
+                        else if(_tool_Abs(air_joy.SWC - 2000) < 50)
+                        {
+                            ctrl.friction_ctrl = FRICTION_ON_MODE;
+                            ctrl.shoot_ctrl = SHOOT_ON;
+                        }
+                    }
+                } 
+
+            #else
+                /*===========================================================*/
+                //环方案            环方案      (已废弃)   
+                else if(_tool_Abs(air_joy.SWB - 2000) < 50)
+                {
+                    ctrl.twist.angular.z = 0;
+                    ctrl.robot_crtl = SHOOT_MODE;                      
+                    if(_tool_Abs(air_joy.SWA - 1000) < 50)  //锁环模式
+                    {
+                        ctrl.chassis_ctrl = CHASSIS_LOCK_RING_MODE;
+                    }
+                    else if(_tool_Abs(air_joy.SWA - 2000) < 50) //环切换
+                    {
+                        ctrl.chassis_ctrl = CHASSIS_TOGGLE_RING_MODE;
+                    }
+
+                    if(_tool_Abs(air_joy.SWD - 1000) < 50)
+                    {
+                        ctrl.pitch_ctrl = PITCH_HAND_MODE;  //手操俯仰
+                    }
+                    else if(_tool_Abs(air_joy.SWD - 2000) < 50)
+                    {
+                        ctrl.pitch_ctrl = PITCH_AUTO_MODE;  //自动俯仰
+                    }
+
+                    if(_tool_Abs(air_joy.SWC - 1000) < 50)
+                    {
+                        ctrl.friction_ctrl = FRICTION_OFF_MODE; //摩擦轮禁用
+                        ctrl.shoot_ctrl = SHOOT_OFF;            //推球禁用
+                    }
+                    else if(_tool_Abs(air_joy.SWC - 1500) < 50)
+                    {
+                        ctrl.friction_ctrl = FRICTION_ON_MODE; //摩擦轮启用
+                        ctrl.shoot_ctrl = SHOOT_OFF;            //推球禁用
+                    }
+                    else if(_tool_Abs(air_joy.SWC - 2000) < 50)
+                    {
+                        ctrl.friction_ctrl = FRICTION_ON_MODE;
+                        ctrl.shoot_ctrl = SHOOT_ON;
+                    }
+                } 
+            #endif
+                /*===========================================================*/
+            
+		    }
+            else//所有机构全部关闭
+            {
+                ctrl.robot_crtl = OFF_MODE;
+                ctrl.chassis_ctrl = CHASSIS_OFF;
+                ctrl.pitch_ctrl = PITCH_RESET_MODE;
+                ctrl.friction_ctrl = FRICTION_OFF_MODE;
+                ctrl.shoot_ctrl = SHOOT_OFF;
+                ctrl.catch_ball = CATCH_OFF;
+                ctrl.car_comm_ctrl = CAR_COMMUICA_OFF;
+                ctrl.laser_ctrl = LASER_CALIBRA_OFF;
+            }
+            xQueueSend(Chassia_Port, &ctrl, 0);
+        }
+        else
+        {
+            ctrl.twist = {0};
+        }
+
+        osDelay(1);
+    }
+}
