3243a28083987d5748419b267ca899f6802545f9
XieFField <3432674139@qq.com>
2025-07-25 17:18:58 +0800

    视觉方式重定位(待测试)

---
+++ R1_VPC/Task/User/Src/relocate_task.cpp
@@ -0,0 +1,42 @@
+/**
+ * @file relocate_task.cpp
+ * @author Wu Jia
+ * @brief 重定位任务
+ * @version 0.1
+ */
+
+
+#include "relocate_task.h"
+
+extern float HOOP_X ;
+extern float HOOP_Y ;
+
+Reposition relocate(HOOP_X, HOOP_Y);
+RELOCATTION_E relocate_signal;
+
+
+
+void relocate_task(void *pvParameters)
+{
+    for(;;)
+    {
+        
+
+        if(xQueueReceive(Relocate_Port, &relocate_signal, pdTRUE) == pdPASS )
+        {
+            //当且仅当锁定yaw轴且发送来重定位信号时候进行重定位
+            if(relocate_signal == BY_VISION)
+            {
+                position2D temp;
+                temp = relocate.Reposition_Calc_VPC(receivey, RealPosData.world_yaw);
+                Reposition_SendData(temp.x, temp.y);
+            }
+            else if(relocate_signal == BY_LASAER)
+            {
+                if(_tool_Abs(RealPosData.world_yaw) < 0.55)
+                    Reposition_SendData(Laser_X_return, Laser_Y_return);
+            }
+        }
+    }
+    osDelay(1);
+}
